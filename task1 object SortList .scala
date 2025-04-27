1da48b7af2b30bad46f5838c78e9df97d40cb438
Anthony sudhan A <122426985+anthony40527@users.noreply.github.com>
2025-04-27 17:18:28 +0530

    Update task1 object SortList .scala

--- task1 object SortList .scala
+++ task1 object SortList .scala
@@ -1,17 +1,16 @@
 object SortList {
-  def sortNumbers(list: List[Int], ascending: Boolean = true): List[Int] = {
-    if (ascending) list.sorted
-    else list.sorted(Ordering[Int].reverse)
-  }
-
   def main(args: Array[String]): Unit = {
-    val numbers = List(5, 2, 8, 1, 9, 3)
-    println("Original List: " + numbers)
+    val numbers = List(4, 1, 7, 3)
 
-    val ascendingSorted = sortNumbers(numbers, ascending = true)
-    println("Ascending Order: " + ascendingSorted)
+    println("Original List:")
+    println(numbers)
 
-    val descendingSorted = sortNumbers(numbers, ascending = false)
-    println("Descending Order: " + descendingSorted)
+    val ascending = numbers.sorted
+    println("Ascending Order:")
+    println(ascending)
+
+    val descending = numbers.sorted(Ordering[Int].reverse)
+    println("Descending Order:")
+    println(descending)
   }
 }
