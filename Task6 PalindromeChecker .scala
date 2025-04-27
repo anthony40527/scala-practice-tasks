3dbba42d10167a8aff4ed2f35392b78df2a3cb4f
Anthony sudhan A <122426985+anthony40527@users.noreply.github.com>
2025-04-27 17:27:27 +0530

    Update Task6 PalindromeChecker .scala

--- Task6 PalindromeChecker .scala
+++ Task6 PalindromeChecker .scala
@@ -7,10 +7,10 @@ object PalindromeChecker {
   }
 
   def checkPalindrome(text: String): Boolean = {
-    // Clean the input by removing spaces, punctuation and converting to lowercase
+   
     val cleanText = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase
     
-    // Check if the cleaned text is equal to its reverse
+   
     cleanText == cleanText.reverse
   }
 }
