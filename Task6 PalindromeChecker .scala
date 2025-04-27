57f6c4217a1b5432bb0b116ceca4c4ac274d49bc
Anthony sudhan A <122426985+anthony40527@users.noreply.github.com>
2025-04-27 20:01:44 +0530

    Update Task6 PalindromeChecker .scala

--- Task6 PalindromeChecker .scala
+++ Task6 PalindromeChecker .scala
@@ -1,16 +1,19 @@
 object PalindromeChecker {
 
   def main(args: Array[String]): Unit = {
-    val inputText = "Never odd or even"
+    println("Enter a text to check if it is a palindrome:")
+
+    val inputText = readLine()
     val isPalindrome = checkPalindrome(inputText)
-    println(s"Is palindrome: $isPalindrome")
+
+    if (isPalindrome)
+      println("It is a palindrome.")
+    else
+      println("It is not a palindrome.")
   }
 
   def checkPalindrome(text: String): Boolean = {
-   
     val cleanText = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase
-    
-   
     cleanText == cleanText.reverse
   }
 }
