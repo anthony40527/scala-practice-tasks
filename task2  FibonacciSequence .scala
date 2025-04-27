5f582c2c588fb0d5e4f4caeb2e3128b11f9b3010
Anthony sudhan A <122426985+anthony40527@users.noreply.github.com>
2025-04-27 17:20:03 +0530

    Update task2  FibonacciSequence .scala

--- task2  FibonacciSequence .scala
+++ task2  FibonacciSequence .scala
@@ -1,19 +1,19 @@
 object FibonacciSequence {
-  def generateFibonacci(n: Int): List[Int] = {
-    if (n <= 0) return List()
-    if (n == 1) return List(0)
-    
-    var fibList = List(0, 1)
-    for (_ <- 2 until n) {
-      val next = fibList.takeRight(2).sum
-      fibList = fibList :+ next
-    }
-    fibList
-  }
-
   def main(args: Array[String]): Unit = {
-    val n = 10
-    val result = generateFibonacci(n)
-    println(s"First $n Fibonacci numbers: $result")
+    val n = 10 
+
+    var first = 0 
+    var second = 1 
+
+    println(s"First $n Fibonacci numbers:")
+
+   
+    for (i <- 1 to n) {
+      print(first + " ")
+
+      val next = first + second 
+      first = second 
+      second = next 
+    }
   }
 }
